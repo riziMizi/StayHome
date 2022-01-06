b807dc90df33dee69fea8a879a486d822fc86343
riziMizi <67862995+riziMizi@users.noreply.github.com>
2022-01-06 23:04:37 +0100

    login prozor

--- app/src/main/java/com/example/stayhome/MainActivity.java
+++ app/src/main/java/com/example/stayhome/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.stayhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtRegister = (TextView) findViewById(R.id.txtRegister);
+        txtRegister.setPaintFlags(txtRegister.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
